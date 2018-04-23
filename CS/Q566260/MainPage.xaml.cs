c146d5efc85d8b446bdf814779d2a41f694dba7f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Q566260/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using DevExpress.Xpf.Grid;
+using DevExpress.Xpf.Printing;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Q566260
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ObservableCollection<Customer> customers = new ObservableCollection<Customer>();
+            for (int i = 1; i < 30; i++)
+            {
+                customers.Add(new Customer() { ID = i, Name = "Name" + i, SomeProperty = "Value" + i });
+            }
+            grid.ItemsSource = customers;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DocumentPreviewWindow preview = new DocumentPreviewWindow();
+            PrintableControlLink link = new PrintableControlLink(grid.View as DevExpress.Xpf.Printing.IPrintableControl);
+            link.ExportServiceUri = "../ExportService1.svc";
+            LinkPreviewModel model = new LinkPreviewModel(link);
+            preview.Model = model;
+            link.CreateDocument(true);
+            preview.ShowDialog();
+        }
+    }
+    public class Customer
+    {
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string SomeProperty
+        {
+            get;
+            set;
+        }
+    }
+
+    public class MyConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            GridColumn column = value as GridColumn;
+            if (column.FieldName == "Name")
+                return new SolidColorBrush(Colors.Red);
+            return new SolidColorBrush(Colors.Green); ;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
---
+++ VB/Q566260/MainPage.xaml.vb
@@ -0,0 +1,89 @@
+﻿Imports Microsoft.VisualBasic
+Imports DevExpress.Xpf.Grid
+Imports DevExpress.Xpf.Printing
+Imports System
+Imports System.Collections.Generic
+Imports System.Collections.ObjectModel
+Imports System.Globalization
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+
+Namespace Q566260
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+
+			Dim customers As New ObservableCollection(Of Customer)()
+			For i As Integer = 1 To 29
+				customers.Add(New Customer() With {.ID = i, .Name = "Name" & i, .SomeProperty = "Value" & i})
+			Next i
+			grid.ItemsSource = customers
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim preview As New DocumentPreviewWindow()
+			Dim link As New PrintableControlLink(TryCast(grid.View, DevExpress.Xpf.Printing.IPrintableControl))
+			link.ExportServiceUri = "../ExportService1.svc"
+			Dim model As New LinkPreviewModel(link)
+			preview.Model = model
+			link.CreateDocument(True)
+			preview.ShowDialog()
+		End Sub
+	End Class
+	Public Class Customer
+		Private privateID As Integer
+		Public Property ID() As Integer
+			Get
+				Return privateID
+			End Get
+			Set(ByVal value As Integer)
+				privateID = value
+			End Set
+		End Property
+
+		Private privateName As String
+		Public Property Name() As String
+			Get
+				Return privateName
+			End Get
+			Set(ByVal value As String)
+				privateName = value
+			End Set
+		End Property
+		Private privateSomeProperty As String
+		Public Property SomeProperty() As String
+			Get
+				Return privateSomeProperty
+			End Get
+			Set(ByVal value As String)
+				privateSomeProperty = value
+			End Set
+		End Property
+	End Class
+
+	Public Class MyConverter
+		Implements IValueConverter
+
+		Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
+			Dim column As GridColumn = TryCast(value, GridColumn)
+			If column.FieldName = "Name" Then
+				Return New SolidColorBrush(Colors.Red)
+			End If
+			Return New SolidColorBrush(Colors.Green)
+
+		End Function
+
+		Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+			Return value
+		End Function
+	End Class
+End Namespace
